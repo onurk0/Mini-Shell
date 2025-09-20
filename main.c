f4449cd502011ee6fdf3d00e7b60a24888594b5f
onur <koseo1@tcnj.edu>
2025-09-19 20:13:03 -0400

    pushing

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void init(Stack *stack){
     stack->top = -1;
 }
 int isEmpty(Stack *stack){
-    return stack->top == 0;
+    return stack->top == -1;
 }
 int isFull (Stack *stack){
     return stack->top == STACK_SIZE - 1;
@@ -27,7 +27,11 @@ int isFull (Stack *stack){
 
 void push(Stack *stack, char *str) {
     if (isFull(stack)) {
-        return;
+        free(stack->command[0]);
+        for (int i = 1; i <= stack->top; i++) {
+            stack->command[i - 1] = stack->command[i];
+        }
+        stack->top--;
     }
     stack->command[++stack->top] = malloc(strlen(str) + 1);
     strcpy(stack->command[stack->top], str);
@@ -78,6 +82,17 @@ int main(void) {
             break;
         }
 
+        if (strcmp(input, "!!\n") == 0) {
+            if (isEmpty(&stack)) {
+                printf("No recent commands\n");
+                continue;
+            }
+            else {
+                char *last_cmd = peek(&stack);
+                strcpy(input, last_cmd);
+            }
+        }
+
         int i = 0;                               // parse the input into args (tokenize)
         char *token = strtok(input, " \t\n");
         while(token != NULL) {
@@ -98,22 +113,10 @@ int main(void) {
                 if(chdir(args[1]) != 0) {
                     printf("%s: %s: No such file or directory\n", args[0], args[1]);
                 }
-                chdir(args[1]);
             }
             continue;
         }
-        else {
-            p = fork();
-        }
 
-        if (strcmp(args[0], "!!") == 0) {
-            if (isEmpty(&stack) == 1) {
-                printf("No recent commands!\n");
-            }
-            else {
-                continue; // IMPLEMENT!
-            }
-        }
         // if the last command is &, then remove it from the arguments
         // and the parent will not call wait(), thus triggering a background process
         if (*args[i-1] == '&') {               
@@ -125,6 +128,7 @@ int main(void) {
             background = 0;
         }
 
+        p = fork();
          // command execution begins w/ fork()
         if (p < 0) {                            // fork failed
             fprintf(stderr, "fork failed\n");
@@ -139,10 +143,8 @@ int main(void) {
         else {
             // parent process - will wait until child is done executing
             // unless '&' is entered
-            if (background == 1) {
-                continue;
-            }
-            wait(NULL);
+            if (!background) 
+              wait(NULL);
         }
     }
     return 0;
